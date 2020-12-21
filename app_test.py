abca37df5fee3938eae9dd198e59ddf4035a9021
Axel Engström <axelen@kth.se>
2020-12-21 20:25:53 +0100

    GitHub actions workflow (#1)
    * Added github actions workflow.
    
    * Updated tag format because build crash
    
    * Don't try to push the test image.
    
    * PEP8
    
    * Just use python directly instead of Dockerfile.tests
    
    * Add deploy job for image upload to registry
    
    * Fixed missing 'runs-on' attribute.

--- app_test.py
+++ app_test.py
@@ -1,12 +1,14 @@
 import pytest
 from app import app as flask_app
 
+
 @pytest.fixture
 def app():
     yield flask_app
 
+
 def test_hello_world(app):
     with app.test_client() as client:
-      res = client.get('/hello')
-      assert res.status_code == 200
-      assert res.get_data(as_text=True) == "Hello, World!"
+        res = client.get('/hello')
+        assert res.status_code == 200
+        assert res.get_data(as_text=True) == "Hello, World!"
